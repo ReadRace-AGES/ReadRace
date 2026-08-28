4fb3898706e6482c4314fe3d3e3e9940e285d309
ViniciusPetersen <viniciuspetersen03@gmail.com>
2026-08-28 20:22:02 -0300

    adição do prettier, que padronização codigo

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = 
-{
-  content: ['./App.tsx', './src/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
+module.exports = {
+  content: ["./App.tsx", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
