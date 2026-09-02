4d7c3af7cf989e873fc86209feda6d85c5669b29
Vitor Moreira Batista <141881488+vitormoreira232@users.noreply.github.com>
2026-09-02 13:35:44 -0300

    1 commit
    1 commit falta testar direito

---
+++ mobile/src/app/(tabs)/meus-livros.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function MeusLivrosScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-bold">Meus Livros</Text>
+    </View>
+  );
+}
